0e8c3409d25d2ea007e3602a90e666e49b40ea67
beginal <beginal01@gmail.com>
2020-10-08 11:27:30 +0900

    UPDATE: test.js 파일 수정

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function example() {
-  const newArr = Array(50).fill()
+  const newArr = Array(50).fill().map((v,i) => i+1).filter((v) => !(v % 2));
+  return newArr;
 }
 
-
-console.log()
+console.log(example())
